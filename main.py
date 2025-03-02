c99593e62b20ee3d48f5333e302cfa4995363b84
d3lvx <m4j3r@poton.me>
2025-03-02 12:13:14 -0500

    creation of main, and skeleton of script

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+
+# Gets the content of a book
+def get_book_text(filePath):
+    with open(filePath) as f:
+        fileContent = f.read()
+        return fileContent
+
+def main():
+    # Prompts user for relative path to book for analysis
+    book = get_book_text(input("Enter relative path to book: "))
+    print(book)
+
+main()
