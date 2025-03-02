2a7430597ceba3ef6d19fac12f340844e8fc054f
d3lvx <m4j3r@poton.me>
2025-03-02 14:00:24 -0500

    created stats.py, added functionality to count individual alphabetic characters and sort them

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+# Gets the content of a book and count of all the words
 
-# Gets the content of a book
+from stats import book_word_count, book_char_count, sort_dicts
+
+#bookPath = input("Enter relative path to book: ") This is to remove hardcoded paths to books
+bookPath = "books/frankenstein.txt"
+
 def get_book_text(filePath):
     with open(filePath) as f:
         fileContent = f.read()
-        return fileContent
+        return fileContent  
+
 
 def main():
-    # Prompts user for relative path to book for analysis
-    book = get_book_text(input("Enter relative path to book: "))
-    print(book)
+
+    book = get_book_text(bookPath)
+    # Grabs total word count from book
+    wordCount = book_word_count(book)
+    # Counts of individual characters
+    charCount = book_char_count(book)
+    #Sorts and returns the character counts, takes charCount dicitionary as input
+    dictSorted = sort_dicts(charCount)
+
+    print("=========== BookBot ===========")
+    print(f"Analyzing the book located at {bookPath}...\n")
+    print("--------- Word Count ---------")
+    print(f"Found {wordCount} total words\n")
+    print("------ Character Count ------")
+    #print(f"{charCount}\n")
+    for entry in dictSorted:
+        print(f"{entry["character"]}: {entry["count"]}")
+
 
 main()
---
+++ stats.py
@@ -0,0 +1,44 @@
+# Splits the book into a list and counts the individual words
+def book_word_count(book):
+    splitWords = book.split()
+    count = 0
+    for word in splitWords:
+        count += 1
+    return count
+
+# Counts the individual characters present in the book
+def book_char_count(book):
+    character_count = {}
+
+    for character in book:
+        if str(character.lower()) in character_count:
+            character_count[character.lower()] += 1
+
+        else:
+            character_count[character.lower()] = 1
+    
+    return character_count
+
+
+# Iterates over the dictionary with characters and counts, and splits the individual \
+# character and corresponding count into its own dictionary and then sorted by 'count' 
+def sort_dicts(dict):
+    list_of_dicts = []
+    
+    for key, value in dict.items():
+
+        # We only want alphabetical characters
+        if key.isalpha():
+            temp_dict = {}
+            temp_dict["character"]= key
+            temp_dict["count"]=value
+            list_of_dicts.append(temp_dict)
+
+    # Returns the counts value of a character
+    def sort_on(d):
+        return d["count"]
+    
+    # Sorts the list of dictionaries by the character counts 'sort_on' function
+    list_of_dicts.sort(reverse=True, key=sort_on)
+
+    return list_of_dicts
